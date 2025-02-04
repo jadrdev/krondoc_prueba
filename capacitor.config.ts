cbd77f3eba664f9d388530eb245e0d4a9f1405fa
Joshua A. Díaz Robayna <jdiarob@saosl.com>
2025-02-04 08:32:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'krondoc_app_v3',
+  webDir: 'www'
+};
+
+export default config;
